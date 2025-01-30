4f3d4e00aba9b2027935ed00a77e614017233715
Amana Qureshi <amanaqureshi@Amanas-Air.attlocal.net>
2025-01-30 17:03:40 -0600

    Update for Module 4

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,5 @@
-"use strict";
-
-(() => {
-    document.querySelector("#testButton").addEventListener("click", handleClick);
-
-function handleClick() {
-    console.log("Thank you for clicking.");
-}
-})();
+(function () {
+    "use strict"
+  
+  })();
+  
