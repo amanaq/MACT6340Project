595f1cea12a4e5c5d24131a3858ba9f689ed27ef
Amana Qureshi <amanaqureshi@Amanas-Air.lan>
2025-03-06 19:12:42 -0600

    updated featured page, projects, and app and index folders as well as NFT art page to show info on art

--- app.js
+++ app.js
@@ -1,55 +1,69 @@
 import express from "express";
+import * as utils from "./utils/utils.js";
+import * as db from "./utils/database.js";
 import dotenv from "dotenv";
 dotenv.config();
-import * as db from './utils/database.js';
-let data = ["Project 1", "Project 2", "Project 3"];
+import cors from "cors";
+
 let projects = [];
 
 const app = express();
+app.use(cors());
 const port = 3000;
 app.set("view engine", "ejs");
-app.use (express.json());
+app.use(express.json());
 app.use(express.static("public"));
 
-
 app.get("/", async (req, res, next) => {
-   await db
-   .connect()
-   .then(async() => {
-    // query the database for project records
-    projects = await db.getAllProjects();
-    console.log(projects);
-    res.render("index.ejs");
-   })
-   .catch(next); 
+  await db
+    .connect()
+    .then(async () => {
+      // query the databse for project records
+      projects = await db.getAllProjects();
+      console.log(projects);
+      let featuredRand = Math.floor(Math.random() * projects.length);
+      res.render("index.ejs", { featuredProject: projects[featuredRand] });
+    })
+    .catch(next);
 });
 
 app.get("/projects", (req, res) => {
-  console.log(projects);
-  res.render("projects.ejs", {projectArray: projects});
- });
-
+  res.render("projects.ejs", { projectArray: projects });
+});
 
 app.get("/project/:id", (req, res) => {
   let id = req.params.id;
-  if (id > data.length) {
+  if (id > projects.length) {
     throw new Error("No project with that ID");
   }
-  res.render("project.ejs", {projectArray: data, which: id });
+  console.log(projects[id - 1], id);
+  res.render("project.ejs", { project: projects[id - 1], which: id });
 });
 
 app.get("/contact", (req, res) => {
   res.render("contact.ejs");
 });
 
-app.post("/mail", async (req, res) => { 
+app.post("/mail", async (req, res) => {
+  await utils
+    .sendMessage(req.body.sub, req.body.txt)
+    .then(() => {
+      res.send({ result: "success" });
+    })
+    .catch(() => {
+      res.send({ result: "failure" });
+    });
 });
-
 
 app.use(async (err, req, res, next) => {
   console.log(err);
-  res.render("error.ejs");
-  
+  let msg;
+  msg = err.message;
+  if (msg != "No project with that ID") {
+    msg =
+      "There was an internal error. Apologies. We are working on cleaning up the mess.";
+  }
+  res.render("error.ejs", { msg: msg });
 });
 
 app.listen(port, () => {
